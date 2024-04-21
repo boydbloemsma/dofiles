847078326493fb8c4f3dc2bcb2dc7a15b7fad351
Boyd Bloemsma <boydbloemsma@gmail.com>
2024-04-21 13:07:20 +0200

    Added random anime wallpaper

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -34,4 +34,37 @@ config.keys = {
     },
 }
 
+local animes = {
+    "lycoreco.jpg",
+    "frieren.jpg",
+    "mdud.jpg",
+}
+
+local anime_base_path = ""
+if wezterm.target_triple:find("darwin") ~= nil then
+    anime_base_path = "/Users/boydbloemsma/.anime/"
+end
+
+config.window_background_image = anime_base_path .. animes[math.random(#animes)]
+config.window_background_image_hsb = {
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.05,
+
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  hue = 1.0,
+
+  -- You can adjust the saturation also.
+  saturation = 1.0
+}
+config.text_background_opacity = 0.7
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+
 return config 
