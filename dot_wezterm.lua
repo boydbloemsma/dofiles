c8d625bdbb10478a121a246c29ff0ce5404e434b
Boyd Bloemsma <boydbloemsma@gmail.com>
2024-05-14 19:17:01 +0200

    Fixed image scaling

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -40,24 +40,28 @@ local animes = {
     "mdud.jpg",
 }
 
-local anime_base_path = ""
+local anime_base_path = "/home/boyd/.anime/"
 if wezterm.target_triple:find("darwin") ~= nil then
     anime_base_path = "/Users/boydbloemsma/.anime/"
 end
 
-config.window_background_image = anime_base_path .. animes[math.random(#animes)]
-config.window_background_image_hsb = {
-  -- Darken the background image by reducing it to 1/3rd
-  brightness = 0.05,
-
-  -- You can adjust the hue by scaling its value.
-  -- a multiplier of 1.0 leaves the value unchanged.
-  hue = 1.0,
-
-  -- You can adjust the saturation also.
-  saturation = 1.0
+local dimmer = {
+    brightness = 0.1,
+    hue = 1,
+    saturation = 1,
 }
-config.text_background_opacity = 0.7
+
+config.background = {
+    {
+        source = {
+            File = anime_base_path .. animes[math.random(#animes)],
+        },
+        height = "Cover",
+        hsb = dimmer,
+    }
+}
+
+config.text_background_opacity = 1
 
 config.window_padding = {
   left = 0,
